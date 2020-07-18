660a4b4c5e0ac74a5d445ec4c923854fa899b742
mastermindsintl <54694501+mastermindsintl@users.noreply.github.com>
2020-07-18 11:04:46 -0400

    Branch 8865

--- tdm_installer/Constants.h
+++ tdm_installer/Constants.h
@@ -1,6 +1,6 @@
 #pragma once
 
-#define TDM_INSTALLER_VERSION "0.16"
+#define TDM_INSTALLER_VERSION "0.17"
 
 #define TDM_INSTALLER_LOG_FILENAME "tdm_installer.log"
 #define TDM_INSTALLER_CONFIG_FILENAME "tdm_installer.ini"
@@ -9,6 +9,7 @@
 #define TDM_INSTALLER_ZIPSYNC_DIR ".zipsync"
 #define TDM_INSTALLER_MANICACHE_SUBDIR "mani"
 #define TDM_INSTALLER_LASTSCAN_PATH TDM_INSTALLER_ZIPSYNC_DIR "/lastscan.ini"
+#define TDM_INSTALLER_LASTINSTALL_PATH TDM_INSTALLER_ZIPSYNC_DIR "/lastinstall.ini"
 
 #define TDM_INSTALLER_CONFIG_URL "http://darkmod.taaaki.za.net/zipsync/tdm_installer.ini"
 #define TDM_INSTALLER_EXECUTABLE_URL_PREFIX "http://darkmod.taaaki.za.net/zipsync/"

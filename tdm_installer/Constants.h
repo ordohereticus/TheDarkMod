b89b21c05d7d93d80323d695ca6cefaa78284527
mastermindsintl <54694501+mastermindsintl@users.noreply.github.com>
2020-07-20 14:40:29 -0400

    Branch 8879

--- tdm_installer/Constants.h
+++ tdm_installer/Constants.h
@@ -1,6 +1,6 @@
 #pragma once
 
-#define TDM_INSTALLER_VERSION "0.18"
+#define TDM_INSTALLER_VERSION "0.19"
 
 #define TDM_INSTALLER_LOG_FILENAME "tdm_installer.log"
 #define TDM_INSTALLER_CONFIG_FILENAME "tdm_installer.ini"
@@ -17,3 +17,13 @@
 
 #define TDM_INSTALLER_FREESPACE_MINIMUM 100				//100 MB --- hardly enough even for differential update
 #define TDM_INSTALLER_FREESPACE_RECOMMENDED (5<<10)		//5 GB --- enough for any update, since it is larger than size of TDM
+
+#define TDM_DARKMOD_CFG_FILENAME "darkmod.cfg"
+#define TDM_DARKMOD_SHORTCUT_NAME "TheDarkMod"
+#define TDM_DARKMOD_SHORTCUT_ICON "TDM_icon.ico"
+#define TDM_DARKMOD_SHORTCUT_COMMENT "TheDarkMod: Stealth Gaming in a Gothic Steampunk World"
+#ifdef _WIN32
+#define TDM_DARKMOD_SHORTCUT_EXECUTABLES {"TheDarkModx64.exe", "TheDarkMod.exe"}
+#else
+#define TDM_DARKMOD_SHORTCUT_EXECUTABLES {"thedarkmod.x64", "thedarkmod.x86"}
+#endif
